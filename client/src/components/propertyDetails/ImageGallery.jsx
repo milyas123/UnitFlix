2d42f01b98f78b89a3f0a1375a30f8b9da310822
Muhammad Anees <aneese421@gmail.com>
2024-07-08 13:00:59 +0500

    Added modal functionality to ImageGallery component with image navigation and closing features

--- client/src/components/propertyDetails/ImageGallery.jsx
+++ client/src/components/propertyDetails/ImageGallery.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import { BsArrowUpRight, BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { X } from "lucide-react";
+
+const images = [
+  "/assets/imgs/p1.jpg",
+  "/assets/imgs/p2.jpg",
+  "/assets/imgs/p3.jpg",
+  "/assets/imgs/p4.jpg",
+  "/assets/imgs/p5.jpg",
+  "/assets/imgs/p6.jpg",
+  "/assets/imgs/p7.jpg",
+  "/assets/imgs/p8.jpg",
+];
+
 const ImageGallery = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="mt-24">
-      <h1 className="font-semibold text-[24px]">Image Gallery</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="font-semibold text-[24px]">Image Gallery</h1>
+
+        <div
+          onClick={openModal}
+          className="cursor-pointer flex items-center gap-1.5 border-b-2 border-transparent hover:border-mirage transition duration-200 ease-in-out"
+        >
+          <p className="text-mirage font-semibold text-[15px]">
+            See All Images
+          </p>
+          <BsArrowUpRight size={20} />
+        </div>
+      </div>
 
       <div className="mt-6 flex">
         <div className="w-[69.5%] space-y-5">
@@ -71,6 +119,37 @@ const ImageGallery = () => {
           />
         </div>
       </div>
+
+      {modalIsOpen && (
+        <div className="fixed z-[600] inset-0 bg-black bg-opacity-70 flex justify-center items-center">
+          <div className="w-full max-w-5xl rounded-lg">
+            <X
+              className="text-white opacity-60 hover:opacity-100 absolute top-6 right-5 cursor-pointer transition-all duration-200 ease-in-out"
+              size={35}
+              onClick={closeModal}
+            />
+            <div className="flex justify-between items-center">
+              <BsChevronLeft
+                size={50}
+                onClick={prevImage}
+                className="pe-6 text-white cursor-pointer"
+              />
+              <div className="w-full h-[500px] overflow-hidden rounded-lg">
+                <img
+                  src={images[currentImageIndex]}
+                  className="object-cover w-full h-full"
+                  alt=""
+                />
+              </div>
+              <BsChevronRight
+                size={47}
+                onClick={nextImage}
+                className="ps-5 text-white cursor-pointer"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
