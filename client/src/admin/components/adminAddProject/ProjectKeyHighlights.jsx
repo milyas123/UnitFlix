ed05ef230137fc56ed5ad947823f02aa7776e5e9
= <=>
2024-10-26 18:23:40 +0500

    Added the new key highlights design to the admin panel

--- client/src/admin/components/adminAddProject/ProjectKeyHighlights.jsx
+++ client/src/admin/components/adminAddProject/ProjectKeyHighlights.jsx
@@ -1,4 +1,3 @@
-import { PiListStar } from "react-icons/pi";
 import Button from "../common/Button";
 import Edit from "@/website/components/svgs/Edit";
 import Delete from "@/website/components/svgs/Delete";
@@ -18,20 +17,18 @@ const ProjectKeyHighlights = ({ formData, showModal, handleEdit, handleDelete })
           Add Highlight
         </Button>
       </div>
-      <div className="mt-4 flex flex-wrap gap-4 md:ms-auto md:mt-0 md:w-[72%]">
+      <div className="mt-4 grid grid-cols-2 gap-4 md:ms-auto md:mt-0 md:w-[72%]">
         {formData?.keyHighlights?.map((highlight, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-center rounded-md border bg-whiteLilac px-2 py-4 text-center md:h-[140px] md:w-[160px] md:p-2 lg:h-[170px] lg:w-[190px] lg:p-2.5 xl:h-[200px] xl:w-[220px] xl:p-3 2xl:h-[270px] 2xl:w-[300px] 2xl:p-4"
-          >
-            <div className="space-y-3 md:space-y-1.5 lg:space-y-2 xl:space-y-3 2xl:space-y-5">
-              <h1 className="user--addProperty-labelTextSize flex items-center justify-center gap-1">
-                <PiListStar size={23} /> {highlight.title}
+          <div key={index}
+               className="w-full flex items-center justify-start rounded-md border bg-whiteLilac p-2.5 text-center">
+            <div className="text-start flex flex-col gap-2">
+              <h1 className="user--addProperty-labelTextSize flex items-center justify-start gap-1">
+                {highlight.title}
               </h1>
               <p className="text-smokeyGrey md:text-[8px] lg:text-[10px] xl:text-[12px] 2xl:text-[14px]">
                 {highlight.description}
               </p>
-              <div className="flex items-center justify-center gap-x-1.5 text-smokeyGrey">
+              <div className="flex items-center justify-end gap-x-1.5 text-smokeyGrey">
                 <Edit onClick={() => handleEdit(index)} className="cursor-pointer" />
                 <Delete onClick={() => handleDelete(index)} className="cursor-pointer" />
               </div>
