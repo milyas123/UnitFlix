b3d449f1e0a25f92ed58d0fc2401fd7ac167b3dc
= <asimwattoo6@gmail.com>
2025-05-02 14:46:27 +0500

    Final changes

--- deployment/scripts/frontend.build.bat
+++ deployment/scripts/frontend.build.bat
@@ -1,5 +1,5 @@
 echo "Building the docker file"
-docker build -f deployment/frontend/Dockerfile --build-arg VITE_SERVER_URL="https://unitflix.com/backend" --build-arg VITE_SITE_KEY="6Le20oIqAAAAAAOa8E1_xxKPXyTc2NyGlVwZaXAV" -t unitflix-frontend .
+docker build -f deployment/frontend/Dockerfile --build-arg VITE_SERVER_URL="https://unitflix.com/backend" --build-arg VITE_SITE_KEY="6LfM14IqAAAAAE5j0K7limY5XtGOXk4cB0CqsJzi" -t unitflix-frontend .
 echo "Tagging the docker file"
 docker tag unitflix-frontend:latest asimwattoo/unitflix-frontend:latest
 echo "Pushing the docker file"
